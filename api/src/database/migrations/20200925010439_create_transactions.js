8b1c57dce7775e484261df807d48f77bffafcaa7
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2020-09-25 02:28:30 -0300

    Update transaction migrate

--- api/src/database/migrations/20200925010439_create_transactions.js
+++ api/src/database/migrations/20200925010439_create_transactions.js
@@ -6,11 +6,9 @@ exports.up = function (knex) {
     table.decimal("net_value").notNullable();
     table.string("flag_name").notNullable();
     table.string("modality_type").notNullable();
-    table.datetime("date").notNullable();
+    table.timestamp("date").notNullable();
     table.date("avaliable").notNullable();
-    table.integer('user_id').unsigned().notNullable();
 
-    table.foreign("user_id").references("id").inTable("user");
     table.foreign("flag_name").references("name").inTable("flags");
     table.foreign("modality_type").references("type").inTable("modalities");
   });

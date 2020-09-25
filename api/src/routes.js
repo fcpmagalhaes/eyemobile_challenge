5233b2e9c2ef6bc531a661736539bf11c2568e77
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2020-09-25 14:19:21 -0300

    Create new routes to controllers

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,9 +1,19 @@
-const express = require("express");
+const express = require('express');
+const TransactionController = require('./controllers/TransactionController');
+const UserController = require('./controllers/UserController');
+const FlagsController = require('./controllers/FlagsController');
+const ModalitiesController = require('./controllers/ModalitiesController');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  return res.send('API running 🚀');
-});
+routes.get('/', (req, res) => res.send('API running 🚀'));
+routes.get('/flags', FlagsController.index);
+routes.get('/modalities', ModalitiesController.index);
+
+routes.get('/users', UserController.index);
+
+routes.post('/users', UserController.create);
+
+routes.get('/transactions', TransactionController.index);
 
 module.exports = routes;

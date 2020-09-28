8e7a8e5c024df2b36c3e9ed04aeb8f1d817807f2
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2020-09-28 18:18:27 -0300

    Create validations with celebrate

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 const TransactionController = require('./controllers/TransactionController');
 const UserController = require('./controllers/UserController');
 const FlagsController = require('./controllers/FlagsController');
@@ -12,17 +13,91 @@ const portalVerify = require('./middleware/PortalVerify');
 const routes = express.Router();
 
 routes.get('/', (req, res) => res.send('API running 🚀'));
-routes.get('/flags', login, terminalVerify, FlagsController.index);
-routes.get('/modalities', login, terminalVerify, ModalitiesController.index);
 
-routes.get('/users', login, terminalVerify, UserController.index);
-routes.post('/users', UserController.create);
+routes.get(
+  '/flags',
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+    }).unknown(),
+  }),
+  login, portalVerify, FlagsController.index,
+);
 
-routes.get('/transactions', login, portalVerify, TransactionController.index);
-routes.post('/transactions', login, terminalVerify, TransactionController.create);
+routes.get(
+  '/modalities',
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+    }).unknown(),
+  }),
+  login, portalVerify, ModalitiesController.index,
+);
 
-routes.post('/sessions', SessionController.create);
+routes.get(
+  '/users',
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+    }).unknown(),
+  }),
+  login, portalVerify, UserController.index,
+);
 
-routes.get('/transactions/balancer', login, portalVerify, BalancerController.index);
+routes.post(
+  '/users',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      login: Joi.string().required(),
+      password: Joi.string().required().length(6),
+    }),
+  }),
+  UserController.create,
+);
+
+routes.post(
+  '/sessions',
+  SessionController.create,
+);
+
+routes.get(
+  '/transactions',
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+    }).unknown(),
+  }),
+  login, portalVerify, TransactionController.index,
+);
+
+routes.post(
+  '/transactions',
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+    }).unknown(),
+  }),
+  login, terminalVerify,
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      nsu: Joi.string().required(),
+      value: Joi.number().precision(2).required(),
+      flag_name: Joi.string().required(),
+      modality_type: Joi.string().required(),
+      date: Joi.date().required(),
+    }),
+  }),
+  TransactionController.create,
+);
+
+routes.get(
+  '/transactions/balancer',
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+    }).unknown(),
+  }),
+  login, portalVerify, BalancerController.index,
+);
 
 module.exports = routes;

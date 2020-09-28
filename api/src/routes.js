12645496703a155970b6781176741f1858dad1be
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2020-09-27 21:34:11 -0300

    Update routes to add authentication to routes

--- api/src/routes.js
+++ api/src/routes.js
@@ -3,17 +3,23 @@ const TransactionController = require('./controllers/TransactionController');
 const UserController = require('./controllers/UserController');
 const FlagsController = require('./controllers/FlagsController');
 const ModalitiesController = require('./controllers/ModalitiesController');
+const SessionController = require('./controllers/SessionController');
+const login = require('./middleware/Login');
+const terminalVerify = require('./middleware/TerminalVerify');
+const portalVerify = require('./middleware/PortalVerify');
 
 const routes = express.Router();
 
 routes.get('/', (req, res) => res.send('API running 🚀'));
-routes.get('/flags', FlagsController.index);
-routes.get('/modalities', ModalitiesController.index);
+routes.get('/flags', login, terminalVerify, FlagsController.index);
+routes.get('/modalities', login, terminalVerify, ModalitiesController.index);
 
-routes.get('/users', UserController.index);
+routes.get('/users', login, terminalVerify, UserController.index);
 routes.post('/users', UserController.create);
 
-routes.get('/transactions', TransactionController.index);
-routes.post('/transactions', TransactionController.create);
+routes.get('/transactions', login, terminalVerify, TransactionController.index);
+routes.post('/transactions', login, portalVerify, TransactionController.create);
+
+routes.post('/sessions', SessionController.create);
 
 module.exports = routes;

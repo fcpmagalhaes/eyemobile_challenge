9e0395917cbe5ba75e316730483f670c32dfad4c
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2020-10-12 15:54:08 -0300

    Update app to use swaggerUi

--- api/src/app.js
+++ api/src/app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const { errors } = require('celebrate');
+const swaggerUi = require('swagger-ui-express');
 const routes = require('./routes');
+const openApiDocumentation = require('./openApiDocumentation');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
 app.use(errors());
 
 module.exports = app;
